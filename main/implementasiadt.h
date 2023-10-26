9f2b8adfa27f75d1352278255f1a3e773688ce91
Bodleh <tfprince079@gmail.com>
2023-10-26 14:52:31 +0700

    Early stages

---
+++ main/implementasiadt.h
@@ -0,0 +1,13 @@
+#ifndef IMPLEMENTASIADT_H
+#define IMPLEMENTASIADT_H
+
+#include "../lib/wordmachine.c"
+#include "../lib/listdin.c"
+#include "../lib/listlinier.c"
+#include "../lib/liststatik.c"
+#include "../lib/matrix.c"
+#include "../lib/queue.c"
+#include "../lib/stack.c"
+#include "../lib/time.c"
+
+#endif
---
+++ main/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "implementasiadt.h"
+
+int main() {
+
+    return 0;
+}
