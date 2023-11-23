2c0f1de5978fd03803e35027888275de0bae48b7
Bodleh <tfprince079@gmail.com>
2023-11-23 20:28:36 +0700

    fix save for balasan

--- lib/save_balasan.c
+++ lib/save_balasan.c
@@ -5,13 +5,43 @@
 
 
 void fprintf_time(FILE *file, TIME T) {
-    fprintf(file, " %02d:%02d:%02d", Hour(T), Minute(T), Second(T));
+    fprintf(file, "%02d:%02d:%02d", Hour(T), Minute(T), Second(T));
 }
 
 void fprintf_datetime(FILE *file, DATETIME D) {
     fprintf(file, "%d/%d/%d ", Day(D), Month(D), Year(D));
-    fprintf_time(file, D.T);
+    fprintf_time(file, Time(D));
     fprintf(file, "\n");
+}
+
+int countElmtTree(addressTree p) {
+    if (p != NULL) {
+        return 1 + countElmtTree(FirstChild(p)) + countElmtTree(NextSibling(p));
+    }
+}
+
+void displayBalasan(FILE *file, addressTree p, addressTree root, ListKicauan LKicau, int i) {
+    addressTree q;
+    if (p != NULL) {
+        if (p == Root(ListKicauan_ELMT(LKicau, i).Balasan)) {
+            fprintf(file, "-1 %d\n", Id(p));
+        }
+        else {
+            q = getAddressBefore(root, p);
+            fprintf(file, "%d %d\n", Id(q), Id(p));
+        }
+        for (int k = 0; k < Pesan(p).Length; k++) {
+            fprintf(file, "%c", Pesan(p).TabWord[k]);
+        }
+        fprintf(file, "\n");
+        for (int k = 0; k < User(p).Length; k++) {
+            fprintf(file, "%c", User(p).TabWord[k]);
+        }
+        fprintf(file, "\n");
+        fprintf_datetime(file, Datetime(p));
+        displayBalasan(file, FirstChild(p), root, LKicau, i);
+        displayBalasan(file, NextSibling(p), root, LKicau, i);
+    }
 }
 
 void save_balasan(ListKicauan LKicau) {
@@ -20,41 +50,30 @@ void save_balasan(ListKicauan LKicau) {
     addressTree p, q, r;
     file = fopen("file.txt", "w");
 
+    printf("%d\n", count);
     for (int j = 0; j < listLength_ListKicauan(LKicau); j++) {
-        if (Root(ListKicauan_ELMT(LKicau, i).Balasan) != NULL) {
+        if (Root(ListKicauan_ELMT(LKicau, j).Balasan) != NULL) {
             count++;
         }
     }
-    fprintf(file, "%d\n", count);
+    printf("%d\n", count);
+    if (count == 0) {
+        fprintf(file, "%d", count);
+    }
+    else {
+        fprintf(file, "%d\n", count);
+    }
 
-    count = 0;
 
     for (i = 0; i < listLength_ListKicauan(LKicau); i++) {
         p = r = Root(ListKicauan_ELMT(LKicau, i).Balasan); // r always root
         if (p != NULL) {
-            fprintf(file, "%d\n", i);
-            while (p != NULL) {
-                count++;
-                p = LEFT(p);
-                p = RIGHT(p);
-            }
+            fprintf(file, "%d\n", i+1);
+            count = countElmtTree(p);
             fprintf(file, "%d\n", count);
-            p = Root(ListKicauan_ELMT(LKicau, i).Balasan);
-            while (p != NULL) {
-                if (p == Root(ListKicauan_ELMT(LKicau, i).Balasan)) {
-                    fprintf(file, "-1 %d\n", Id(p));
-                }
-                else {
-                    q = getAddressBefore(r, p);
-                    fprintf(file, "%d %d\n", Id(q), Id(p));
-                }
-                fprintf(file, *Pesan(p).TabWord, "\n", *User(p).TabWord, "\n");
-                fprintf_datetime(file, Datetime(p));
-                p = LEFT(p);
-                p = RIGHT(p);
-            }
-            
+            displayBalasan(file, p, r, LKicau, i);
         }
     }
+    fclose(file);
 }
 
